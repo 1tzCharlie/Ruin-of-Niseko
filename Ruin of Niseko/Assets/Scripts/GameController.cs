0fdbd02d49ed40c1d7b5dab4e4bda2b362be1259
1tzCharlie <99755490+1tzCharlie@users.noreply.github.com>
2022-06-25 22:16:12 +0100

    Working on Chest and Items
    Made chest model and made items more interesting visually when on the ground.

---
+++ Ruin of Niseko/Assets/Scripts/GameController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Color pyromancyUIBackground;
+    public Color cryomancyUIBackground;
+    public Color necromancyUIBackground;
+}
--- Ruin of Niseko/Assets/Scripts/UI/ItemUIScript.cs
+++ Ruin of Niseko/Assets/Scripts/UI/ItemUIScript.cs
@@ -6,17 +6,32 @@ using UnityEngine.UI;
 public class ItemUIScript : MonoBehaviour
 {
     GameObject mainItemSlot;
-    public Image itemIcon;
+    Image itemIcon;
+
+    GameObject inventoryManagerObject;
+    InventoryManager inventoryManager;
+
+    ItemObject thisItem;
 
     private void Awake()
     {
         mainItemSlot = GameObject.Find("MainItemIcon");
+
+        inventoryManagerObject = GameObject.Find("InventoryManager");
+        inventoryManager = inventoryManagerObject.GetComponent<InventoryManager>();
     }
 
-    public void UpdateImage()
+    public void UpdateItemAndImage()
     {
         Image itemImage = mainItemSlot.GetComponent<Image>();
 
-        itemImage.sprite = itemIcon.sprite;
+        itemImage.sprite = thisItem.icon;
+
+        inventoryManager.SetMainItem(thisItem);
+    }
+
+    public void UpdateItem(ItemObject item)
+    {
+        thisItem = item;
     }
 }
